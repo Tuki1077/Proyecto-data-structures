f1156c60065326d58663f0b8a3e769d63d52eaa0
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2021-03-01 09:00:30 -0600

    Update mesa.java

--- mesa.java
+++ mesa.java
@@ -1,13 +1,43 @@
-public class mesa{
-    int nopersonas;
-    int conta;
-
-    public mesa(int persona){
-        this.nopersonas = persona;
+class mesa {
+    client clienteAsignado; //cliente en la mesa
+    int cantidad; //cantidad de sillas en la mesa
+    boolean disponibilidad; //Si mesa esta ocupada o no
+    
+    mesa(int cantidad){
+        this.cantidad = cantidad;
+        this.disponibilidad = true;
+        this.clienteAsignado = null;
     }
     
-    @Override
-    public String toString(){
-        return "Mesa de " + nopersonas;
+    //asignar cliente y ocupar la mesa
+    public void assignarClient(client cliente){
+        this.clienteAsignado = cliente;
+        this.disponibilidad = false;
+    }
+    
+    //retirar al cliente y desocupar la mesa
+    public void retirarCliente(){
+        this.clienteAsignado = null;
+        this.disponibilidad  = true;
+    }
+    
+     public int verCantidad(){
+        return cantidad;
+    }
+    
+    //revisar si la mesa esta disponible
+    public boolean esDisponible() {
+        return disponibilidad;
+    }
+    
+    @Override 
+    public String toString() {
+        if(disponibilidad != true){
+            return "Mesa de " + cantidad + " disponible: "+ disponibilidad + " cliente: " + clienteAsignado.toString();
+        }
+        else{
+            return "Mesa de " + cantidad + " disponible: "+ disponibilidad;
+        }
+        
     }
 }
