4b519d8c6a6eb699046599ddfe4c7c14dd82a4bb
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2021-03-01 09:00:54 -0600

    Create client.java

---
+++ client.java
@@ -0,0 +1,19 @@
+class client {
+    String nombre; //nombre del clinete
+    int prioridad; //mas prioridad es mas importante. Esto representa el espacio en la mesa
+    
+    client (String nombre, int prioridad){
+        this.nombre = nombre;
+        this.prioridad = prioridad;
+    }
+    
+    //ver prioridad del cliente
+    public int verPrioridad(){
+        return prioridad;
+    }
+    
+    @Override
+    public String toString() {
+        return "Nombre:" + nombre + " P: " + prioridad;
+    }
+}
