4bae1150880ec57ed218d7ad8be22607703c06fa
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2021-02-28 19:23:38 -0600

    Update mesa.java

--- mesa.java
+++ mesa.java
@@ -1,17 +1,3 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) {
-        List<String> list = new ArrayList<String>();
-        List <String> lista_especial = new ArrayList <String>();
-        List<Integer> suma = new ArrayList <Integer> ();
-        int tam = 0;
-        int nummes = 0;
-        int contador = 0;
-        Scanner input = new Scanner (System.in);
-        boolean condicion1 = true;
-        boolean condicion2 = true;
-        int mesa = 10;
 public class mesa{
     int nopersonas;
     int conta;
